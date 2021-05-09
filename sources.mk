eb53a24d5d1270f78914e7d57fc1efd4efee54e8
PRASANTH <prasanthpp.pp1@gmail.com>
2021-05-09 10:34:33 +0530

    OLED interface fixed

--- sources.mk
+++ sources.mk
@@ -4,12 +4,17 @@ SOURCES =  ./main.c \
            ./timer/timer.c \
            ./uart/uart.c \
            ./gps_module/gps.c \
-           ./nmea_indexing/nmea.c
+           ./nmea_indexing/nmea.c \
+           ./oled/ssd1306.c \
+           ./gui/ugui.c
+           
 	   
 # Include paths
 INCLUDES = -I ./inc/ \
            -I ./timer/ \
            -I ./uart/ \
            -I ./gps_module/ \
-           -I ./nmea_indexing/ 
+           -I ./nmea_indexing/ \
+           -I ./oled/ \
+           -I ./gui/
            
